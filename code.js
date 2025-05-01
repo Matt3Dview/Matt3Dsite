875f6b6a3bc5b1c86be9ae535bf80e768c1ace15
Matt3Dview <botellamatt@gmail.com>
2025-05-01 18:11:29 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,5 @@
+// *** This would contain ALL the JavaScript code from your original p5.js sketch ***
+// ... (All your original functions: preload, setup, draw, mousePressed, etc.)
+
+// Minor adjustment: You don't need createInput() or createButton() in the HTML directly.
+// p5.js will handle placing them on the page when you use createInput() and createButton() in the setup() function.  
